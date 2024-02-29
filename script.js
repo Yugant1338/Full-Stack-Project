866e8d4949bb667a941189d6300aafee81009ee3
Yugant1338 <122391669+Yugant1338@users.noreply.github.com>
2024-03-01 00:50:56 +0530

    Update Index.html

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-let plusBtns = document.querySelectorAll('#faq-for-all');
-let expandedTexts = document.querySelectorAll('.faq .expanded');
-plusBtns.forEach((plus, index) => {
-    plus.addEventListener('click', () => {
-        if(expandedTexts[index].style.visibility==="hidden"){
-            expandedTexts[index].style.visibility = 'visible';
-        }
-        else{
-            expandedTexts[index].style.visibility = 'hidden';
-        }
 
+let faq = document.querySelectorAll('.faq');
+let plusbtn = document.querySelectorAll('span');
+let expanded = document.querySelectorAll('.expanded');
+let addedElements = []; // Store references to added elements
+
+plusbtn.forEach((plus, index) => {
+    plus.addEventListener('click', () => {
+        let addExpanded;
+
+        if (plus.innerText === '+') {
+            plus.innerText = 'X';
+            addExpanded = document.createElement('div');
+            addExpanded.innerHTML = expanded[index].innerHTML;
+            faq[index].after(addExpanded);
+            addedElements[index] = addExpanded; // Store the reference
+        }
+        else if (plus.innerText === 'X' && addedElements[index]) {
+            plus.innerText = '+';
+            addedElements[index].remove(); // Use the stored reference to remove
+        }
     });
 });
