b495bcf986d865dee64740f4ae1fc7c2306df776
MadhuramsinhSolanki <madhuramsinghsolanki@gmail.com>
2024-03-01 17:53:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,9 @@
+const buttons = document.querySelectorAll('.faqinside');
 
-let faq = document.querySelectorAll('.faq');
-let plusbtn = document.querySelectorAll('span');
-let expanded = document.querySelectorAll('.expanded');
-let addedElements = []; // Store references to added elements
+buttons.forEach(button => {
+    button.addEventListener('click', function() {
+        const slide = this.nextElementSibling;
 
-plusbtn.forEach((plus, index) => {
-    plus.addEventListener('click', () => {
-        let addExpanded;
-
-        if (plus.innerText === '+') {
-            plus.innerText = 'X';
-            addExpanded = document.createElement('div');
-            addExpanded.innerHTML = expanded[index].innerHTML;
-            addExpanded.style.color = 'white';
-            addExpanded.style.fontSize = '20px';
-            addExpanded.style.height = '100px';
-            addExpanded.style.textAlign = 'left';
-            addExpanded.style.textIndent = '10px';
-            addExpanded.style.backgroundColor = '#272829';
-            addExpanded.style.display = 'flex';
-            addExpanded.style.flex = '1';
-            addExpanded.style.width = '1000px';
-            addExpanded.style.alignItems = 'center';
-            addExpanded.style.margin = 'auto';
-            addExpanded.style.border = '0';
-
-            faq[index].after(addExpanded);
-            addedElements[index] = addExpanded; // Store the reference
-        }
-        else if (plus.innerText === 'X' && addedElements[index]) {
-            plus.innerText = '+';
-            addedElements[index].remove(); // Use the stored reference to remove
-        }
+        slide.classList.toggle('active');
     });
 });
